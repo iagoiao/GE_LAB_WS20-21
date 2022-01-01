98e53fc6e704d6d2e0bdc46c106ada5d874289ac
OliveiraIago <iago.oliveira@engenharia.ufjf.br>
2022-01-01 19:00:48 +0100

    Simple Password Auth

--- GE_LAB_WEBSITE/app.py
+++ GE_LAB_WEBSITE/app.py
@@ -1,10 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template,request,redirect
+import helper_fxns
+
 app = Flask(__name__)
 
+#Declare Globals
+input_list = []
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template("index.html")
 
 
-#Testing GIT
+@app.route("/check_user",methods = ["POST"])
+def check_user():
+    
+    username = request.form.get('Username_')
+    password = request.form.get('Password_')
+    condition = helper_fxns.check_user(username,password)
+
+    if(condition):
+
+        return redirect("/workspace")
+    else:
+        return redirect("/failure")
+
+
+
+@app.route("/workspace")
+def workspace():
+    return render_template("workspace.html")
+
+@app.route("/failure")
+def failure():
+    return render_template("failure.html")
---
+++ GE_LAB_WEBSITE/helper_fxns.py
@@ -0,0 +1,8 @@
+
+
+def check_user (username,password):# dummy function for tests
+    print('username: ',username,'password: ',password)
+
+    return False
+
+
